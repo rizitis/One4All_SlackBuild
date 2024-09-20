7aaf3360c8f2ef8771f3b60b21ff980db49959a4
Anagnostakis Ioannis <412135+rizitis@users.noreply.github.com>
2024-09-20 19:58:02 +0300

    update

--- Slackware-Commander/package-Slackware-Commander/usr/local/bin/panic/fun1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PS1='C:\\\> '
-
-while true; do
-    read -e -p "C:\\>" input
-    exec "$input" &
-done
--- Slackware-Commander/package-Slackware-Commander/usr/local/bin/panic/fun14.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-trap "echo 'This script cannot be killed with Ctrl+C!'" SIGINT
-trap "" SIGHUP
-
-while true; do
-    echo "Press Ctrl+C to see the message"
-    sleep 1
-done
